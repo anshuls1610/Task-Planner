01980357aa321b23a15a92201d36df5b1fbc2390
Anshul1610 <anshulsharma1610@gmail.com>
2021-08-08 16:34:11 +0530

    init

---
+++ src/views/Role.vue
@@ -0,0 +1,25 @@
+<template>
+  <vnatk-crud :options="crudoptions"> </vnatk-crud>
+</template>
+
+<script>
+import { VnatkCrud } from "vnatk-vue";
+import service from "../services/service";
+
+export default {
+  name: "Role",
+  components: {
+    VnatkCrud,
+  },
+  data() {
+    return {
+      crudoptions: {
+        service: service,
+        basepath: "/api/vnatk",
+        model: "Role",
+        title: "Roles",
+      },
+    };
+  },
+};
+</script>
